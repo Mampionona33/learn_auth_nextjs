bc9768d87e1e10031c41cc078c1d0b03bd188af0
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-25 11:53:39 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // Using server components
-// import { getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 // import { authOptions } from "./api/auth/[...nextauth]/route";
 // import { redirect } from "next/navigation";
 
