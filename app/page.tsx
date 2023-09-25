cf65b2c3dd5288e47e2b0284d80cc367f10ccde1
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-25 06:37:31 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,27 +14,6 @@ const Home = () => {
       )}
     </div>
   );
-  // const { data: session, status } = useSession();
-  // const handleSignin = () => {
-  //   signIn("google");
-  // };
-  // const handleSignout = () => {
-  //   signOut();
-  // };
-  // // this if condition will check if the user is authenticated or not
-  // if (status === "authenticated") {
-  //   return (
-  //     <div>
-  //       <h1>{session.user?.email}</h1>
-  //       <button onClick={handleSignout}>Logout</button>
-  //     </div>
-  //   );
-  // }
-  // return (
-  //   <div>
-  //     <button onClick={handleSignin}>Login Button</button>
-  //   </div>
-  // );
 };
 
 export default Home;
