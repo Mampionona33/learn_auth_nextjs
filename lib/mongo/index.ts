7d59e735e0d0e4fdd2ec87e450db12f5f42ba8c0
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-26 07:11:32 +0000

    Update 7 files : setup connection to mongodb

---
+++ lib/mongo/index.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+const mongodbPassword = process.env.MONGO_PASSWORD || "Arc35b6FHQEIWOqb";
+const URI =
+  process.env.MONGODB_URI ||
+  `mongodb+srv://ramamps33:${mongodbPassword}@cluster0.vkfhid4.mongodb.net/?retryWrites=true&w=majority`;
+
+const options = {};
+
+if (!URI) throw new Error("Please add your Mongo URI to .env local");
+
+console.log(URI);
+let client = new MongoClient(URI, options);
+let clientPromise;
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global._mongoclientPromise) {
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
---
+++ lib/mongo/users.ts
@@ -0,0 +1,39 @@
+import clientPromise from "./index";
+
+class User {
+  private client;
+  private db;
+  private users;
+
+  async init() {
+    if (this.db) return;
+    try {
+      this.client = await clientPromise;
+      this.db = await this.client.db();
+      this.users = await this.db.collection("user");
+    } catch (error) {
+      //   throw new Error( "Failed to stablish connection to database");
+      throw error;
+    }
+  }
+
+  public async getUsers() {
+    try {
+      if (!this.users) await this.init();
+      const users = await this.users.find({});
+      console.log(users);
+      
+      return { users: users };
+    } catch (error) {
+      return { error: "Failed to fetch users" };
+    }
+  }
+
+  constructor() {
+    (async () => {
+      await this.init();
+    })();
+  }
+}
+
+export default User;
