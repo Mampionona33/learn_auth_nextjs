ba261f7cf7711ba829344c2fac31e0a87e46b5b2
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-28 07:03:23 +0000

    Update mongo.ts and users.ts

--- lib/mongo/mongo.ts
+++ lib/mongo/mongo.ts
@@ -5,22 +5,24 @@ class Mongo {
   private db: Db | null = null;
   private client: MongoClient | null = null;
   private collection: Collection | null = null;
-  private collectionName : string ="";
+  private collectionName: string = "";
 
-
-  constructor(collectionName:string) {
-    this.collectionName = collectionName
+  constructor(collectionName: string) {
+    this.collectionName = collectionName;
+    async () => {
+      await this.init;
+    };
   }
 
   public async init() {
-    if(this.db) return;
+    if (this.db) return;
     try {
-        this.client = await clientPromise;
-        this.db = await this.client.db();
-        this.collection = await this.db.collection(this.collectionName)
-        return this.collection;
+      this.client = await clientPromise;
+      this.db = await this.client.db();
+      this.collection = await this.db.collection(this.collectionName);
+      return this.collection;
     } catch (error) {
-        throw error
+      throw error;
     }
   }
 }
--- lib/mongo/users.ts
+++ lib/mongo/users.ts
@@ -1,19 +1,68 @@
+// import { Collection } from "mongodb";
+// import init from "./init";
+// import get from "./get";
+// import Mongo from "./mongo";
+
+// class User {
+//   private collectionName: string = "users";
+//   private users: Collection = {} as Collection;
+//   // private init = async ()=>  {await  init(this.collectionName)};
+//   private mongo = new Mongo(this.collectionName);
+//   private init = async () => await this.mongo.init();
+
+//   public async getAll() {
+//     try {
+//       if (!this.users) await this.init;
+//       const users = await this.users!.find({}).toArray();
+//       return { users: users };
+//     } catch (error) {
+//       return { error: "Failed to fetch users" };
+//     }
+//   }
+
+//   public async getByEmail(userEmail: string | undefined | null) {
+//     const query = { email: userEmail };
+//     return await get(query, this.users);
+//   }
+
+//   // public async getByEmail(userEmail: string | undefined | null) {
+//   //   try {
+//   //     if (!this.users) await this.init;
+//   //     const query = { email: userEmail };
+//   //     const searchUser = await this.users!.findOne(query);
+//   //     return searchUser;
+//   //   } catch (error) {
+//   //     return { error: "Failed to fetch user by email" };
+//   //   }
+//   // }
+
+//   constructor() {
+//     // (async () => {
+//     //   await this.init;
+//     // })();
+//   }
+// }
+
+// export default User;
 import { Collection } from "mongodb";
-import init from "./init";
-import get from "./get";
 import Mongo from "./mongo";
 
 class User {
   private collectionName: string = "users";
-  private users: Collection = {} as Collection;
-  // private init = async ()=>  {await  init(this.collectionName)};
-  private mongo = new Mongo(this.collectionName);
-  private init = async () => await this.mongo.init();
+  private users: Collection | null = null;
+  private mongo: Mongo;
+
+  constructor() {
+    this.mongo = new Mongo(this.collectionName);
+  }
 
   public async getAll() {
     try {
-      if (!this.users) await this.init;
-      const users = await this.users!.find({}).toArray();
+      if (!this.users) {
+        await this.mongo.init();
+        this.users = this.mongo.getCollection();
+      }
+      const users = await this.users.find({}).toArray();
       return { users: users };
     } catch (error) {
       return { error: "Failed to fetch users" };
@@ -23,23 +72,6 @@ class User {
   public async getByEmail(userEmail: string | undefined | null) {
     const query = { email: userEmail };
     return await get(query, this.users);
-  }
-
-  // public async getByEmail(userEmail: string | undefined | null) {
-  //   try {
-  //     if (!this.users) await this.init;
-  //     const query = { email: userEmail };
-  //     const searchUser = await this.users!.findOne(query);
-  //     return searchUser;
-  //   } catch (error) {
-  //     return { error: "Failed to fetch user by email" };
-  //   }
-  // }
-
-  constructor() {
-    (async () => {
-      await this.init;
-    })();
   }
 }
 
