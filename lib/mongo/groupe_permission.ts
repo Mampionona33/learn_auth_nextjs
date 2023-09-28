f6eb13e0dc40ed20ead0a8a93da7867de1738b56
Mampionona33 <ramamps33@gmail.com>
2023-09-28 12:38:58 +0000

    change in groupe_permission

--- lib/mongo/groupe_permission.ts
+++ lib/mongo/groupe_permission.ts
@@ -1,29 +1,39 @@
 import { Collection, ObjectId } from "mongodb";
 import init from "./init";
+import Mongo from "./mongo";
 
 class Groupe_permission {
   
   private groupe_permission: Collection | null = null;
   private collectionName : string = "groupe_permission";
-  private init = async ()=>await init(this.collectionName);
+  // private init = async ()=>await init(this.collectionName);
+
+  private mongo:Mongo;
 
   public async get(groupeId: string | undefined | null) {
     try {
-      if (!this.groupe_permission) await this.init();
-      const query = { groupe: new ObjectId(groupeId!) };
-      const searchGroupePermission = await this.groupe_permission!.find(
-        query
-      ).toArray();
-      return searchGroupePermission;
+      const groupe_permission = await this.mongo.get({groupe:groupeId});
+      return groupe_permission;
     } catch (error) {
-      return { error: "Failed to fetch groupe_permission by groupeId" };
+      return {error:  `Failed to fetch groupe_permission by${groupeId}`}
     }
+    // try {
+    //   if (!this.groupe_permission) await this.init();
+    //   const query = { groupe: new ObjectId(groupeId!) };
+    //   const searchGroupePermission = await this.groupe_permission!.find(
+    //     query
+    //   ).toArray();
+    //   return searchGroupePermission;
+    // } catch (error) {
+    //   return { error: "Failed to fetch groupe_permission by groupeId" };
+    // }
   }
 
   constructor() {
-    (async () => {
-      await this.init;
-    })();
+    this.mongo = new Mongo(this.collectionName);
+    // (async () => {
+    //   await this.init;
+    // })();
   }
 }
 
