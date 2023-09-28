296fb14eed722826a6ce4382aa28618cfb2cabc7
Mampionona33 <ramamps33@gmail.com>
2023-09-28 21:39:19 +0300

    trying to setup permission api

--- app/api/permission/route.ts
+++ app/api/permission/route.ts
@@ -1,10 +1,13 @@
 import Permission from "@/lib/mongo/permission";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET() {
-  const fetchPermissions = new Permission();
-  try {
-    
-  } catch (error) {
-    
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const permission = new Permission();
+  if (req.method == "GET") {
+    console.log("test");
+    // console.log(await permission.fetch());
   }
 }
