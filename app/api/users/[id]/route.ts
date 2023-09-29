12b0a64b05d718e77047d67aff8bb562445bc51f
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-29 09:21:59 +0000

    finish get user by in api

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,16 +1,17 @@
-import Groupe from "@/lib/mongo/groupe";
+import User from "@/lib/mongo/users";
 import { ObjectId } from "bson";
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
 export async function GET(req: NextApiRequest, { params }) {
-  const userHandler = new Groupe();
+  const userHandler = new User();
   const id = params.id;
   const query = { _id: new ObjectId(id) };
   if (req.method == "GET") {
     try {
       const user = await userHandler.fetch(query);
-      return NextResponse.json({ user: user });
+      console.log(user);
+      return NextResponse.json({ user });
     } catch (error) {
       return NextResponse.json({ error: error });
     }
