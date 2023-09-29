83df26fe4d15651c82da0ba6ed5504ac2eaf08a6
Mampionona33 <ramamps33@gmail.com>
2023-09-29 07:26:45 +0000

    remove id

--- app/api/groupe/route.ts
+++ app/api/groupe/route.ts
@@ -2,13 +2,11 @@ import Groupe from "@/lib/mongo/groupe";
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, { params }) {
+export async function GET(req: NextApiRequest) {
   const groupe = new Groupe();
-  const id = params.id;
-  const query = { _id: new ObjectId(id) };
   if (req.method == "GET") {
     try {
-      const groupes = await groupe.fetch(query);
+      const groupes = await groupe.fetch();
       return NextResponse.json({ groupes });
     } catch (error) {
       return NextResponse.json({ error: error });
