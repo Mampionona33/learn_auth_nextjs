a3e8113fcfdba3b44398d1267b378b1f4e403e11
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-29 07:16:13 +0000

    tes

--- app/api/groupe/route.ts
+++ app/api/groupe/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: NextApiRequest) {
   const groupe = new Groupe();
-
   if (req.method == "GET") {
     try {
       const groupes = await groupe.fetch();
