220e1046bd691f20f214dfe5b84e5f40bc3c487c
Mampionona33 <ramamps33@gmail.com>
2023-09-29 21:12:16 +0300

    clean signin page

--- app/api/auth/signin/page.tsx
+++ app/api/auth/signin/page.tsx
@@ -5,18 +5,15 @@ import { redirect } from "next/navigation";
 function SignIn() {
   const { data: session } = useSession();
 
-  // Vérifiez si l'utilisateur est déjà connecté
   if (session) {
-    redirect.push("/"); // Remplacez '/tableau-de-bord' par le chemin de votre choix
-    return null; // Assurez-vous de renvoyer null pour éviter le rendu de la page de connexion
+    redirect("/");
+    return null;
   }
   // Custom Login Page in Next-Auth and Next.js 13 Authentication : Complete Step-by-Step Tutorial
   // https://www.youtube.com/watch?v=hADeo48SATU
-  // Le reste du contenu de votre page de connexion par défaut
   return (
     <div>
       <h1>Page de connexion</h1>
-      {/* Autres éléments de la page de connexion */}
     </div>
   );
 }
