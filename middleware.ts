a044891c3a6cba801be95931502e7200eb6b7d4e
Mampionona33 <ramamps33@gmail.com>
2023-10-01 14:27:22 +0300

    change in middleware

--- middleware.ts
+++ middleware.ts
@@ -6,52 +6,8 @@ export default withAuth(async function middleware(req: NextRequestWithAuth) {
   const pathname = req.nextUrl.pathname;
   const basedUrl = req.nextUrl;
 
-  try {
-    if (token) {
-      const userResponse = await fetch(
-        `${process.env.NEXTAUTH_URL}/api/users/${token._id}`
-      );
-
-      if (!userResponse.ok) {
-        console.error(
-          `Fetching user role failed with status ${userResponse.status}`
-        );
-        return NextResponse.error();
-      }
-
-      const user = await userResponse.json();
-
-      const userLogged = user.user;
-      if (userLogged && userLogged.length > 0) {
-        const groupeId = userLogged[0].groupe;
-
-        const groupeResponse = await fetch(
-          `${process.env.NEXTAUTH_URL}/api/groupe/${groupeId}`
-        );
-
-        if (!groupeResponse.ok) {
-          console.error(
-            `Fetching groupe failed with status ${groupeResponse.status}`
-          );
-          return NextResponse.error();
-        }
-
-        const groupe = await groupeResponse.json();
-
-        const groupeName = await groupe.groupes[0].name;
-
-        // if (groupeName.match(/responsable/gi) && pathname === "/") {
-        //   const respHomePage = new URL("/dashboard", req.url);
-        //   return NextResponse.redirect(respHomePage);
-        // }
-        if (pathname === "/") {
-          const respHomePage = new URL("/dashboard", req.url);
-          return NextResponse.redirect(respHomePage);
-        }
-      }
-    }
-  } catch (error) {
-    console.error("Error while fetching user role:", error);
-    return NextResponse.error();
+  if (req.nextauth?.token?.email && req.nextUrl.pathname === "/") {
+    const dashboardPage = new URL("/dashboard", req.url);
+    return NextResponse.redirect(dashboardPage);
   }
 });
