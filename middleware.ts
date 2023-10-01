3fc9dec4cda126ef4acb6b2cdeb33d1c6774f07c
Mampionona33 <ramamps33@gmail.com>
2023-10-01 18:16:26 +0300

    test

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,23 @@
 import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 export default withAuth(async function middleware(req: NextRequestWithAuth) {
   const token = req.nextauth.token;
   const pathname = req.nextUrl.pathname;
   const basedUrl = req.nextUrl;
+  const prisma = new PrismaClient();
 
   if (req.nextauth?.token?.email && req.nextUrl.pathname === "/") {
     const dashboardPage = new URL("/dashboard", req.url);
     return NextResponse.redirect(dashboardPage);
   }
+  // try {
+  //   const user = await prisma.users.findUnique({
+  //     where: { email: token?.email },
+  //   });
+  //   console.log("user:", user);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 });
