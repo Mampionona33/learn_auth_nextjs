40453501010f5174673b59f830f20aa53db803cf
Mampionona33 <ramamps33@gmail.com>
2023-10-02 09:42:55 +0000

    test

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -12,10 +12,34 @@ import { NextRequestWithAuth } from "next-auth/middleware";
 //   }
 // }
 
+// export async function GET(req: NextRequestWithAuth) {
+//   if (req.method == "GET") {
+//     try {
+//       const users = await prisma.users.findMany();
+//       const result = NextResponse.json({ users });
+//       return result;
+//     } catch (error) {
+//       return NextResponse.json({ error });
+//     }
+//   }
+// }
+
 export async function GET(req: NextRequestWithAuth) {
-  if (req.method == "GET") {
+  if (req.method === "GET") {
+    const { email } = req.nextUrl.searchParams
+    console.log(email);
+    
+
+    if (!email || typeof email !== "string") {
+      return NextResponse.json({ error: "Invalid email parameter" });
+    }
+
     try {
-      const users = await prisma.users.findMany();
+      const users = await prisma.users.findUnique({
+        where: {
+          email: email,
+        },
+      });
       const result = NextResponse.json({ users });
       return result;
     } catch (error) {
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,45 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import {  useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
-import prisma from "@/lib/prisma/prisma";
 
 
-
-const Dashboard = () => {
+const Dashboard =  () => {
   const { data: session, status } = useSession();
-  console.log(session)
-
+  const [userData, setUserData] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+  
   if (status === "unauthenticated") {
     redirect("/api/auth/signin");
     return null;
   }
+
+  useEffect(() => {
+      if (session) {
+      // Check if user's email is available
+      if (session.user?.email) {
+        fetch(`/api/users/65127ffde4e55c82c6ada8e7`)
+          .then((res) => res.json())
+          .then((data) => {
+            setUserData(data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("Error fetching user data:", error);
+            setLoading(false);
+          });
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [session]);
+
+  if (isLoading) return <p>Loading...</p>
+  if (!userData) return <p>No profile data</p>
+
+  // console.log(userData);
  
   return (
     <>
