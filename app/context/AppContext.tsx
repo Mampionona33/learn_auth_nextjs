3cd217cccf34e12cd2e5fee434d4c8a847a059c7
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-10-04 05:24:22 +0000

    Update AppContext.tsx

--- app/context/AppContext.tsx
+++ app/context/AppContext.tsx
@@ -1,21 +1,39 @@
-import { ReactNode, createContext, useContext, useMemo, useState } from "react";
+import { createContext, useContext, useReducer, useMemo } from "react";
 
-const AppContext = createContext({});
+const AppContext = createContext();
 
-export function AppContextWrapper({ children }: { children: ReactNode }) {
-  const [appState, setAppState] = useState({});
+const initialState = {
+  user: null,
+};
+
+export const ActionTypes = {
+  SET_USER: "SET_USER",
+};
+
+// Créez un réducteur pour gérer l'état de l'application
+const appReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case ActionTypes.SET_USER:
+      return { ...state, user: payload };
+    default:
+      return state;
+  }
+};
+
+export function AppContextProvider({ children }) {
+  const [appState, dispatch] = useReducer(appReducer, initialState);
 
   const contextValue = useMemo(() => {
-    return { appState, setAppState };
-  }, [appState, setAppState]);
+    return { appState, dispatch };
+  }, [appState]);
 
   return (
-    <AppContext.Provider value={contextValue}>
-      {children}
-    </AppContext.Provider>
-  )
+    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+  );
 }
 
-export function useAppContext(){
+// Créez un hook personnalisé pour accéder au contexte
+export function useAppContext() {
   return useContext(AppContext);
 }
