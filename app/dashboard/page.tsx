35d6c2057ef968bf3609a4f17646b105d610462f
Mampionona33 <ramamps33@gmail.com>
2023-10-05 09:59:52 +0000

    create hooks

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,75 +1,36 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
 import Card from "../components/Card";
-import { ActionTypes, useAppContext } from "../context/AppContext";
 import useGetUserLoggedGroupe from "../hook/useGetUserLoggedGroupe";
+import useGetUserData from "../hook/useGetUserData";
 
 const Dashboard = () => {
-  const { data: session, status } = useSession();
-  const [userData, setUserData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-  const { dispatch, appState } = useAppContext();
-  const { userGroupe, loading, error } = useGetUserLoggedGroupe();
+  const {
+    userData,
+    error: errorLoadingUserData,
+    loading: loadingUserData,
+  } = useGetUserData();
+  const {
+    userGroupe,
+    loading: loadingUserloggedGroupe,
+    error: errorOnGetUserGroupe,
+  } = useGetUserLoggedGroupe();
 
-  useEffect(() => {
-    let mount = true;
-
-    const fetchData = async () => {
-      try {
-        if (session?.user?.id && !appState.user) {
-          const res = await fetch(`/api/users/${session.user.id}`);
-          if (!res.ok) {
-            throw new Error('Failed to fetch user data');
-          }
-          const data = await res.json();
-
-          if (mount) {
-            setUserData(data);
-            setLoading(false);
-            if (!appState.user) {
-              dispatch({ type: ActionTypes.SET_USER, payload: data.user });
-            }
-          }
-        } else {
-          if (mount) {
-            setLoading(false);
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-
-        if (mount) {
-          setLoading(false);
-        }
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      mount = false;
-    };
-  }, [session, appState, dispatch]);
-
-  if (status === "unauthenticated") {
-    redirect("/api/auth/signin");
-    return null;
+  if (loadingUserData || loadingUserloggedGroupe) {
+    return <p>Loading ...</p>;
   }
 
-  if (isLoading) return <p>Loading...</p>;
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
+  if (errorLoadingUserData) {
+    return <p>${errorLoadingUserData}</p>;
   }
 
-  if (!appState.user) return <p>No profile data</p>;
+  if (errorOnGetUserGroupe) {
+    return <p>${errorOnGetUserGroupe}</p>;
+  }
 
   return (
     <>
-      {session ? (
+      {userData ? (
         <div className="col-md-9 ml-sm-auto col-lg-10 p-4">
           <Card />
           <pre>{JSON.stringify(userGroupe, null, 2)}</pre>
@@ -78,6 +39,5 @@ const Dashboard = () => {
     </>
   );
 };
-
 
 export default Dashboard;
--- app/hook/useGetUserData.tsx
+++ app/hook/useGetUserData.tsx
@@ -1,38 +1,49 @@
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useAppContext } from "../context/AppContext";
-import { useSession } from "next-auth/react";
+import { ActionTypes, useAppContext } from "../context/AppContext";
+import { IUser } from "../context/interfaceUser";
 
 const useGetUserData = () => {
-  const [userInfo, setUserInfo] = useState(null);
+  const [userData, setUserData] = useState<IUser | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { dispatch, appState } = useAppContext();
   const { data: session, status } = useSession();
 
   useEffect(() => {
+    let mount = true;
     const fetchData = async () => {
       try {
-        if (session?.user?.id && !appState.user) {
-          const res = await fetch(`/api/users/${session.user.id}`);
-          if (!res.ok) {
-            throw new Error("Failed to fetch user data");
-          }
-          const data = await res.json();
 
-          if (mount) {
-            setUserData(data);
-            setLoading(false);
-            if (!appState.user) {
-              dispatch({ type: ActionTypes.SET_USER, payload: data.user });
+        if (session?.user?.id) {
+          if (!appState.user) {
+            const res = await fetch(`/api/users/${session.user.id}`);
+            if (!res.ok) {
+              throw new Error("Failed to fetch user data");
+            }
+            const data = await res.json();
+
+            if (mount) {
+              setUserData(data);
+              setLoading(false);
+              if (!appState.user) {
+                dispatch({ type: ActionTypes.SET_USER, payload: data.user });
+              }
+            }
+          } else {
+            if (mount) {
+              setUserData(appState.user);
+              setLoading(false);
             }
           }
+
         } else {
           if (mount) {
             setLoading(false);
           }
         }
-      } catch (error) {
+      } catch (error:any) {
+        setError(error)
         console.error("Error fetching user data:", error);
 
         if (mount) {
@@ -48,7 +59,7 @@ const useGetUserData = () => {
     };
   }, [session, appState, dispatch]);
 
-  return { userInfo, loading, error };
+  return { userData, loading, error };
 };
 
 export default useGetUserData;
