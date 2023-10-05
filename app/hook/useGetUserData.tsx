6268a4279171cef5a72b656964cd5ac99b7ebaab
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-10-05 09:08:07 +0000

    Update useGetUserData.tsx

--- app/hook/useGetUserData.tsx
+++ app/hook/useGetUserData.tsx
@@ -1,17 +1,54 @@
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useAppContext } from "../context/AppContext";
+import { useSession } from "next-auth/react";
 
 const useGetUserData = () => {
   const [userInfo, setUserInfo] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { dispatch, appState } = useAppContext();
+  const { data: session, status } = useSession();
 
-  useEffect(()=>{
-    
-  })
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (session?.user?.id && !appState.user) {
+          const res = await fetch(`/api/users/${session.user.id}`);
+          if (!res.ok) {
+            throw new Error("Failed to fetch user data");
+          }
+          const data = await res.json();
 
+          if (mount) {
+            setUserData(data);
+            setLoading(false);
+            if (!appState.user) {
+              dispatch({ type: ActionTypes.SET_USER, payload: data.user });
+            }
+          }
+        } else {
+          if (mount) {
+            setLoading(false);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+
+        if (mount) {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      mount = false;
+    };
+  }, [session, appState, dispatch]);
+
+  return { userInfo, loading, error };
 };
 
 export default useGetUserData;
