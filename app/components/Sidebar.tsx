ce3083bafdaa4a70e005cdae4cd0bdb67d57d211
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-10-05 12:22:50 +0000

    Update Sidebar.tsx and tailwind.config.ts

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -24,14 +24,13 @@ const Sidebar = () => {
   return (
     <>
       {session && !isLoading ? (
-        // <div className="col-md-2 d-none d-md-block bg-light flex flex-col p-0 fixed top-0 ">
         <div className="bg-light flex-md-nowrap p-0 shadow min-h-min h-[100rem]">
           <div className="sidebar pt-6 block">
             <div className="sidebar-sticky">
               <ul className="nav flex-column">
-                <li className="nav-item pl-1 hover:bg-slate-400 ">
+                <li className="nav-item pl-1 hover:bg-slate-400 hover:text-white-important">
                   <Link
-                    className="text-capitalize text-decoration-none text-dark d-block hover:text-white "
+                    className="text-capitalize text-decoration-none text-dark d-block"
                     href="/dashboard"
                   >
                     dashboard
@@ -49,8 +48,7 @@ const Sidebar = () => {
             </div>
           </div>
         </div>
-      ) : // </div>
-      null}
+      ) : null}
     </>
   );
 };
