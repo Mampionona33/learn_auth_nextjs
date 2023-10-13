fc5e7aa0f313c59b5993f8c8eb1c7f67b1659195
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-10-13 11:02:39 +0000

    Update userActions.ts

--- app/store/users/userActions.ts
+++ app/store/users/userActions.ts
@@ -1,8 +1,11 @@
 import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchUsers = createAsyncThunk("users/fetchAll", async () => {
-  const res = await fetch(`/api/users?page=1&limit=5`);
-  const data = await res.json();
-  console.log("userActions", data);
-  return data;
-});
+export const fetchUsers = createAsyncThunk(
+  "users/fetchAll",
+  async ({ page = 1, limit = 5 }) => {
+    const res = await fetch(`/api/users?page=${page}&limit=${limit}`);
+    const data = await res.json();
+    console.log("userActions", data);
+    return data;
+  },
+);
