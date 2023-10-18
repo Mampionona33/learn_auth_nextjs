0745f9dd2fd4803f204a0559bb25c88ad546b5a4
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-10-18 10:39:31 +0000

    Update CustomModal_1.tsx and ModalAddUser.tsx

--- app/components/CustomModal_1.tsx
+++ app/components/CustomModal_1.tsx
@@ -1,18 +1,19 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from "react";
 
 type FieldConfig = {
   type: string;
   id: string;
   placeholder: string;
   required?: boolean;
-}
+  name?: string; // Add name property for radio buttons
+};
 
 const CustomModal_1 = ({
   id,
   title,
   body,
   fields,
-  handleSubmit
+  handleSubmit,
 }: {
   id: string;
   title: string;
@@ -20,24 +21,32 @@ const CustomModal_1 = ({
   fields: FieldConfig[];
   handleSubmit: (formData: { [key: string]: string }) => void;
 }) => {
-	const formRef = useRef(null);
+  const formRef = useRef(null);
+  const [selectedOptions, setSelectedOptions] = useState({});
 
-	const handleFormSubmit =(ev)=>{
-		ev.preventDefault();
-		const form = ev.target as HTMLFormElement;
+  const handleFormSubmit = (ev) => {
+    ev.preventDefault();
+    const form = ev.target as HTMLFormElement;
 
-	 	const formData: { [key: string]: string } = {};
+    const formData: { [key: string]: string } = {};
 
-	  for (let i = 0; i < form.elements.length; i++) {
-	    const element = form.elements[i] as HTMLInputElement;
+    for (let i = 0; i < form.elements.length; i++) {
+      const element = form.elements[i] as HTMLInputElement;
 
-	    if (element.tagName === 'INPUT') {
-	      formData[element.id] = element.value;
-	    }
-	  }
+      if (element.tagName === "INPUT") {
+        if (element.type === "radio" && element.checked) {
+          formData[element.name] = element.value;
+        } else if (element.type !== "radio") {
+          formData[element.id] = element.value;
+        }
+      }
+      if (element.tagName === "SELECT") {
+        formData[element.id] = selectedOptions[element.id];
+      }
+    }
 
-  	console.log(formData);
-	}
+    console.log(formData);
+  };
 
   return (
     <>
@@ -46,41 +55,145 @@ const CustomModal_1 = ({
           <div className="modal-content">
             <div className="modal-header">
               <h5>{title}</h5>
-              <button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button
+                className="btn-close"
+                type="button"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <form onSubmit={handleFormSubmit}>
-            	
-	            <div className="modal-body">
-	              {body ? body : null}
-	              {fields && fields.length > 0 ? (
-	                <div className="row g-3">
-	                  {fields.map((item, key) => (
-	                    <div className="col-md-6" key={key}>
-	                      <label htmlFor={item.id} className="visually-hidden">{item.placeholder}</label>
-	                      <input
-	                        type={item.type}
-	                        className="form-control"
-	                        id={item.id}
-	                        placeholder={item.placeholder}
-	                        required={item.required}
-	                      />
-	                    </div>
-	                  ))}
-	                </div>
-	              ) : null}
-	            </div>
-	            <div className="modal-footer">
-	              <button type="button" data-bs-dismiss="modal" className="btn btn-secondary">Annuler</button>
-	              {/*<button type="button" className="btn btn-primary" onClick={handleSubmit}>Valider</button>*/}
-	              <input type="submit" value="Save" />
-	            </div>
-            
+              <div className="modal-body">
+                {body ? body : null}
+                {fields && fields.length > 0 ? (
+                  <div className="row g-3">
+                    {fields.map((item, key) => {
+                      // Initialize selectedOption here
+                      if (item.type === "select") {
+                        if (!selectedOptions[item.id]) {
+                          setSelectedOptions({
+                            ...selectedOptions,
+                            [item.id]: item.options[0],
+                          });
+                        }
+                      }
+                      return (
+                        <div className="col-md-6" key={key}>
+                          <label htmlFor={item.id} className="visually-hidden">
+                            {item.placeholder}
+                          </label>
+
+                          {(() => {
+                            switch (item.type) {
+                              case "text":
+                              case "password":
+                              case "email":
+                                return (
+                                  <input
+                                    type={item.type}
+                                    className="form-control"
+                                    id={item.id}
+                                    placeholder={item.placeholder}
+                                    required={item.required}
+                                  />
+                                );
+
+                              case "textarea":
+                                return (
+                                  <textarea
+                                    className="form-control"
+                                    id={item.id}
+                                    placeholder={item.placeholder}
+                                    required={item.required}
+                                  />
+                                );
+
+                              case "select":
+                                return (
+                                  <div className="input-group mb-3">
+                                    <label
+                                      htmlFor={item.id}
+                                      className="input-group-text"
+                                    >
+                                      {item.placeholder}
+                                    </label>
+                                    <select
+                                      className="form-control"
+                                      id={item.id}
+                                      required={item.required}
+                                      value={selectedOptions[item.id]}
+                                      onChange={(e) => {
+                                        setSelectedOptions({
+                                          ...selectedOptions,
+                                          [item.id]: e.target.value,
+                                        });
+                                      }}
+                                    >
+                                      {item.options.map((option, index) => (
+                                        <option key={index} value={option}>
+                                          {option}
+                                        </option>
+                                      ))}
+                                    </select>
+                                  </div>
+                                );
+
+                              case "radio":
+                                return (
+                                  <div className="input-group mb-3">
+                                    <span className="input-group-text">
+                                      {item.placeholder}
+                                    </span>
+                                    <div className="d-flex gap-2">
+                                      {item.options.map((option, index) => (
+                                        <div key={index} className="form-check">
+                                          <input
+                                            className="form-check-input"
+                                            type="radio"
+                                            name={item.name} // Le nom du groupe de boutons radio
+                                            id={`${item.id}-${index}`}
+                                            value={option}
+                                          />
+                                          <label
+                                            className="form-check-label"
+                                            htmlFor={`${item.id}-${index}`}
+                                          >
+                                            {option}
+                                          </label>
+                                        </div>
+                                      ))}
+                                    </div>
+                                  </div>
+                                );
+
+                              default:
+                                return null;
+                            }
+                          })()}
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : null}
+              </div>
+
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  data-bs-dismiss="modal"
+                  className="btn btn-secondary"
+                >
+                  Annuler
+                </button>
+                {/*<button type="button" className="btn btn-primary" onClick={handleSubmit}>Valider</button>*/}
+                <input type="submit" className="btn btn-primary" value="Save" />
+              </div>
             </form>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default CustomModal_1;
--- app/components/ModalAddUser.tsx
+++ app/components/ModalAddUser.tsx
@@ -1,71 +1,114 @@
 import CustomModal from "@components/CustomModal";
 import CustomModal_1 from "@components/CustomModal_1";
 
-
-const FormAddUser = ( ) =>{
-	return(
-		<>
-			<div className="row g-3">
-		      <div className="col-md-6">
-		        <label htmlFor="name" className="visually-hidden">
-		          name
-		        </label>
-		        <input type="text" className="form-control" id="name" placeholder="Name" />
-		      </div>
-		      <div className="col-md-6">
-		        <label htmlFor="lastname" className="visually-hidden">
-		          lastname
-		        </label>
-		        <input type="text" className="form-control" id="lastname" placeholder="Lastname" />
-		      </div>
-		      <div className="col-md-6">
-		        <label htmlFor="username" className="visually-hidden">
-		          Username
-		        </label>
-		        <input type="text" className="form-control" id="username" placeholder="Username" />
-		      </div>
-		      <div className="col-md-6">
-		        <label htmlFor="password" className="visually-hidden">
-		          Password
-		        </label>
-		        <input type="password" className="form-control" id="password" placeholder="Password" />
-		      </div>
-		      <div className="col-md-6">
-		        <label htmlFor="email" className="visually-hidden">
-		          Email
-		        </label>
-		        <input type="email" className="form-control" id="email" placeholder="Email" />
-		      </div>
-		      <div className="col-md-6">
-		        <label htmlFor="phone" className="visually-hidden">
-		          Email
-		        </label>
-		        <input type="text" className="form-control" id="phone" placeholder="Phone" />
-		      </div>
-		      <div className="col-md-6">
-	  			{/*placeholder liste groupe*/}
-		      </div>
-		    </div>
-		</>
-	)
-}
-
+const FormAddUser = () => {
+  return (
+    <>
+      <div className="row g-3">
+        <div className="col-md-6">
+          <label htmlFor="name" className="visually-hidden">
+            name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Name"
+          />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="lastname" className="visually-hidden">
+            lastname
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastname"
+            placeholder="Lastname"
+          />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="username" className="visually-hidden">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            placeholder="Username"
+          />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="password" className="visually-hidden">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="email" className="visually-hidden">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="phone" className="visually-hidden">
+            Email
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="phone"
+            placeholder="Phone"
+          />
+        </div>
+        <div className="col-md-6">{/*placeholder liste groupe*/}</div>
+      </div>
+    </>
+  );
+};
 
 const fields = [
-	{type:'text', id:"name", placeholder:"Name", required:true },
-	{type:'text', id:"username", placeholder:"Username", required:true },
-	{type:'password', id:"password", placeholder:"Password", required:true},
-	{type:'email', id:"email", placeholder:"Email"},
-	{type:'text', id:"phone", placeholder:"Phone"},
+  { type: "text", id: "name", placeholder: "Name", required: true },
+  { type: "text", id: "username", placeholder: "Username", required: true },
+  { type: "password", id: "password", placeholder: "Password", required: true },
+  { type: "email", id: "email", placeholder: "Email" },
+  { type: "text", id: "phone", placeholder: "Phone" },
+  {
+    type: "select",
+    id: "groupe",
+    placeholder: "Groupe",
+    options: ["admin", "responsable", "student"],
+  },
+  {
+    type: "radio",
+    name: "sex",
+    id: "sex",
+    placeholder: "Sex",
+    options: ["Male", "female"],
+  },
 ];
 
-const ModalAddUser = () =>{
-	return(
-		<>
-		 {/*<CustomModal title="Ajout utilisateur" body={<FormAddUser/>} id="modaAddUser" />*/}
-		 <CustomModal_1 title="Ajout utilisateur" fields={fields} id="modaAddUser" />
-		</>
-	)
-}
+const ModalAddUser = () => {
+  return (
+    <>
+      {/*<CustomModal title="Ajout utilisateur" body={<FormAddUser/>} id="modaAddUser" />*/}
+      <CustomModal_1
+        title="Ajout utilisateur"
+        fields={fields}
+        id="modaAddUser"
+      />
+    </>
+  );
+};
 
 export default ModalAddUser;
